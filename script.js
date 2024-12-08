a66ba71f87bb7cdf697d24e581d6bbb3c08baa4d
xomexo <51759830+xomexo@users.noreply.github.com>
2024-12-08 18:38:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function scaleFunction() {
+    document.getElementById("project").style.transform="scale(1.1)";
+    setTimeout(()=>{
+        document.getElementById("project").style.transform="scale(1)";
+    }, 500)
+}
+
+function scaleFunction2() {
+    document.getElementById("project2").style.transform="scale(1.1)";
+    setTimeout(()=>{
+        document.getElementById("project2").style.transform="scale(1)";
+    }, 500)
+}
+
+function scaleFunction3() {
+    document.getElementById("project3").style.transform="scale(1.1)";
+    setTimeout(()=>{
+        document.getElementById("project3").style.transform="scale(1)";
+    }, 500)
+}
